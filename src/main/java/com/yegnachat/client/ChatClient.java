49a1d603152ac4a8b97f34c0f7cd429bbe535ef8
Hiruy <hiruyhabtamu12@gmail.com>
2025-12-17 17:08:06 +0300

    Minor file insertion/removal

--- src/main/java/com/yegnachat/client/ChatClient.java
+++ src/main/java/com/yegnachat/client/ChatClient.java
@@ -12,7 +12,7 @@ public class ChatClient extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/yegnachat/client/login.fxml"));
         Scene scene = new Scene(loader.load());
-        stage.setTitle("YegnaChat — Login");
+        stage.setTitle("Login");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
